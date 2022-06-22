f4dfeea83b5dacc7a4b68fdc75527c9ebfec8d8c
kimsungwon4 <skim@uppgo.jp>
2022-06-22 17:01:39 +0900

    init add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {useMemo, useState} from 'react'
+import Test from './components/Test'
+
+const hardCalculate = (number) => {
+  console.log('어려운 계산!!');
+  for (let i = 0; i < 999999999 ; i++) {
+  }
+
+  return number + 10000;
+}
+
+const easyCalculate = (number) => {
+  console.log('짱 쉬운 계산!!');
+  return number + 1;
+}
 
 function App() {
+  const [hardNumber, setHardNumber] = useState(1)
+  const [easyNumber, setEasyNumber] = useState(1)
+
+  // const hardSum = hardCalculate(hardNumber);
+  const hardSum = useMemo(() => {
+    return hardCalculate(hardNumber)
+  }, [hardNumber])
+  const easySum = easyCalculate(easyNumber);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>어려운계산기</h3>
+      <input type="number" value={hardNumber} onChange={(e) => setHardNumber(parseInt(e.target.value))} />
+      <span> + 10000 = {hardSum}</span>
+      <br />
+      <h3>쉬운계산기</h3>
+      <input type="number" value={easyNumber} onChange={(e) => setEasyNumber(parseInt(e.target.value))} />
+      <span> + 1 = {easySum}</span>
+      <br /><br /><br />
+      <hr />
+      <Test />
     </div>
   );
 }
